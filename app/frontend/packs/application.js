683a7748cf0c703c371d485ce583098394b5ef42
StanTim <1694569@mail.ru>
2022-01-01 21:03:49 +0300

    Add bootstrap

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -7,7 +7,7 @@ import Rails from "@rails/ujs"
 
 Rails.start()
 
-import '../styles/application'
+import '../styles/application.scss'
 const images = require.context('../images', true)
 
 import 'bootstrap/dist/js/bootstrap'
