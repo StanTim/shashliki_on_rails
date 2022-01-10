d479db57ae93a424cdc4842867f7700b72c148bd
StanTim <1694569@mail.ru>
2022-01-10 15:58:39 +0300

    add attributes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,8 +12,8 @@ class ApplicationController < ActionController::Base
   # Настройка для девайза — разрешаем обновлять профиль, но обрезаем
   # параметры, связанные со сменой пароля.
   def configure_permitted_parameters
-    attributes = [:name, :email, :avatar]
-    devise_parameter_sanitizer.permit( :account_update, keys: [:password, :password_confirmation, :current_password])
+    attributes = [:name, :email, :avatar, :password, :password_confirmation, :current_password]
+    devise_parameter_sanitizer.permit(:sign_up, keys: attributes)
     devise_parameter_sanitizer.permit(:account_update, keys: attributes)
   end
 
