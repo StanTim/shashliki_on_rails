619f347889c556865807d5d6eb1bd86e95ee0420
StanTim <1694569@mail.ru>
2022-01-11 10:55:30 +0300

    dell bootstrap_flash method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,14 +3,4 @@ module ApplicationHelper
   def user_avatar(user)
     asset_pack_path('media/images/user.png')
   end
-  #
-  # def bootstrap_flash(alert)
-  #   {
-  #     success: "alert-success",
-  #     error: "alert-danger",
-  #     danger: "alert-danger",
-  #     alert: "alert-warning",
-  #     notice: "alert-info"
-  #   }[alert.to_sym] || alert
-  # end
 end
