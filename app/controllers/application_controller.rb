3be344527aafbda556e23fcc5897c791b99ee7f1
StanTim <1694569@mail.ru>
2022-01-11 08:20:19 +0300

    add sign_up attributes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,6 +14,7 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     attributes = %i[name email password password_confirmation current_password]
     devise_parameter_sanitizer.permit(:sign_up, keys: attributes)
+    devise_parameter_sanitizer.permit(:sign_in, keys: attributes)
     devise_parameter_sanitizer.permit(:account_update, keys: attributes)
   end
 
