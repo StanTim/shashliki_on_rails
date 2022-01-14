dc3eca6e5807ce99b9817fc1998ad7dbbbc3ce12
StanTim <1694569@mail.ru>
2022-01-14 18:36:22 +0300

    del not used views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,14 @@ module ApplicationHelper
   def user_avatar(user)
     asset_pack_path('media/images/user.png')
   end
+
+  def flash_class(level)
+    case level
+      when :notice then "alert alert-info"
+      when :success then "alert alert-success"
+      when :error then "alert alert-error"
+      when :alert then "alert alert-error"
+    end
+  end
+
 end
