040a9f8fd399e8492ebf45835f2e67e11a4e6ff1
StanTim <1694569@mail.ru>
2022-04-03 10:41:20 +0300

    add delete link for photos

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -1,18 +1,14 @@
 import Rails from "@rails/ujs"
+
 import "bootstrap/dist/js/bootstrap"
 
 import "../styles/application.scss"
-import Lightbox from 'bs5-lightbox'
+import "../styles/lightbox.css"
+
+import "./lightbox"
 
 Rails.start()
 
 const images = require.context("../images", true)
 
 require("jquery")
-
-// document.querySelectorAll('.my-lightbox-toggle').forEach((el) => el.addEventListener('click',
-//   (e) => {
-//     e.preventDefault();
-//     const lightbox = new Lightbox(el);
-//     lightbox.show();
-//   }));
